34e69fe5512ee56dd094675dffc985e282d4700d
Daymer Perdomo <daimer003@gmail.com>
2025-09-19 14:10:51 -0500

    feat: loading url video

--- src/components/loading/Loading.vue
+++ src/components/loading/Loading.vue
@@ -1,5 +1,5 @@
 <script setup>
-import loaderVideo from "@/assets/video/load-boda.mp4";
+
 </script>
 
 <template>
@@ -9,7 +9,7 @@ import loaderVideo from "@/assets/video/load-boda.mp4";
       <video autoplay loop muted playsinline class="h-[100%] w-[100%] object-cover
                [mask-image:radial-gradient(ellipse_at_center,rgba(0,0,0,1)_45%,transparent_100%)]
                [mask-repeat:no-repeat] [mask-position:center] [mask-size:cover]">
-        <source :src="loaderVideo" type="video/mp4" />
+        <source src="https://res.cloudinary.com/diccp2984/video/upload/v1758308487/load-boda_mjzo75.mp4" type="video/mp4" />
       </video>
 
       <div class="absolute bottom-0 w-full h-1/3 flex items-end justify-center
